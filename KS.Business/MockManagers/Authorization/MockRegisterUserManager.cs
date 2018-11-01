735327b169174190f2f31e21c648de4a9e492169
Ben Arnold <bena@knowledgeservices.com>
2018-11-01 14:39:00 -0400

    Unit testing

---
+++ KS.Business/MockManagers/Authorization/MockRegisterUserManager.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using KS.Business.DataContract.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KS.Business.MockManagers.Authorization
+{
+    public class MockRegisterUserManager : IRegisterUserManager
+    {
+        public Task<bool> ReturnValue { get; set; }
+        public int CallCount { get; set; }
+
+        public async Task<bool> RegisterUser(NewUserCreateDTO userDTO)
+        {
+            CallCount++;
+            return await ReturnValue;
+        }
+    }
+}
---
+++ KS.UnitTests/RegisterControllerTests.cs
@@ -0,0 +1,56 @@
+using AutoMapper;
+using KS.API;
+using KS.API.Controllers.Authorization;
+using KS.API.DataContract.Authorization;
+using KS.Business.DataContract.Authorization;
+using KS.Business.MockManagers.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace KS.UnitTests
+{
+    [TestClass]
+    public class RegisterControllerTests
+    {
+        private RegisterController _controller;
+        private MockRegisterUserManager _manager;
+
+        [TestInitialize]
+        public void Arrange()
+        {
+            var mappingConfig = new MapperConfiguration(mc =>
+            {
+                mc.AddProfile(new MappingProfile());
+            });
+
+            IMapper mapper = mappingConfig.CreateMapper();
+            _manager = new MockRegisterUserManager{ReturnValue = Task.FromResult(true)};
+            _controller = new RegisterController(_manager, mapper);
+        }
+
+        [TestMethod]
+        public void RegisterController_Register_ShouldHitManager()
+        {
+            var userRequest = new NewUserCreateRequest {UserName = "Ben", Password = "Stuff"};
+
+            var result = _controller.Register(userRequest);
+
+            Assert.AreEqual(1, _manager.CallCount);
+        }
+
+        [TestMethod]
+        public void RegisterController_Register_ShouldReturnStatusCode()
+        {
+            var userRequest = new NewUserCreateRequest { UserName = "Ben", Password = "Stuff" };
+
+            var actual = _controller.Register(userRequest).Result;
+            var expected = (int)HttpStatusCode.Created;
+  
+
+            Assert.AreEqual<HttpStatusCode>(expected, actual);
+        }
+    }
+}
